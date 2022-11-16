d5bbaf9881429ae2bc79edc4ff4e43c21af7c908
Kim Yurim <110047935+luvulmi@users.noreply.github.com>
2022-11-17 01:41:55 +0900

    zz

---
+++ Mypage/App.js
@@ -0,0 +1,13 @@
+// import MyPoint from './pages/MyPoint'
+import './App.css';
+import MyAddress from './pages/myAddress';
+
+function App() {
+  return (
+    <>
+      <MyAddress />
+    </>
+  );
+}
+
+export default App;
---
+++ Mypage/components/UserAddressList.js
@@ -0,0 +1,14 @@
+export default function UserAddressList({ userData }) {
+  return (
+    <>
+      {userData.map((item, idx) => (
+        <tr key={item + idx}>
+          <td>기본</td>
+          <td>{item.name}</td>
+          <td>{item.address}</td>
+          <td>{item.number}</td>
+          <td>삭제</td>
+        </tr>))}
+    </>
+  );
+}
