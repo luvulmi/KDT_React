0bbe3cddf97c1b16a0e72b089d350db8043ac701
Kim Yurim <110047935+luvulmi@users.noreply.github.com>
2022-11-17 18:22:22 +0900

    mypage

---
+++ src_mypage/App.js
@@ -0,0 +1,13 @@
+// import MyPoint from './pages/MyPoint'
+import './App.css';
+import MyAddress from './pages/MyAddress';
+
+function App() {
+  return (
+    <>
+      <MyAddress />
+    </>
+  );
+}
+
+export default App;
